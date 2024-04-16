559b7751eeb1362ad43dd81aa6eef57ba34e9aff
Davi Ferreira da Silva <sildv45@gmail.com>
2024-04-16 10:47:03 -0300

    Criei O Header a aplicação e fiz a parte de segurnça do login

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,8 @@
+import { Header } from "../../components/Header";
 export function Admin() {
   return (
-    <div>
-      <h1>Página Admin</h1>
+    <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+      <Header />
     </div>
   );
 }
